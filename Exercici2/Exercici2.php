7ea698b71236880063322ad1482423cafca72f09
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-09-29 17:27:25 +0200

    Exercici 2 Sense fet a mig

---
+++ Exercici2/Exercici2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $floors = array();
+        for ($i = 0; $i < 5; $i++) {
+            $rooms = array();
+            $floors[i] = $rooms;
+            for ($j = 0; $j < 10; $j++) {
+                
+            }
+        }
+    ?>
+</body>
+</html>
